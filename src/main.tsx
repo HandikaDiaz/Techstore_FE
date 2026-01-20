04e6f507c89523dfaa36b87e91c4fbeef5f39332
Alexa <handikaalexandriadiaz@gmail.com>
2026-01-20 07:13:01 +0700

    Add LogRocket

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AuthProvider from './layouts/authLayout.tsx'
+import LogRocket from 'logrocket';
+
+LogRocket.init(import.meta.env.VITE_LOGROCKET_ID);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
